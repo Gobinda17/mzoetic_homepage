2adaa65eff820e3fe3a8993a914dc80286595035
Gobinda17 <gobindadeb14@gmail.com>
2024-03-20 20:08:55 +0530

    Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,26 @@
 <template>
-    <h1>
-        Navbar
-    </h1>
+    <div class="flex items-center bg-slate-500 p-[15px]">
+        <img class="w-10" :src="logoImg" alt="Logo" />
+        <p class="text-salte-900">
+            <i class="ri-tent-line"></i> stay
+            <i class="ri-caravan-line"></i> tour
+            <i class="ri-treasure-map-line"></i> adventure
+            <i class="ri-motorbike-fill"></i> ride
+            <i class="ri-car-line"></i> rentals
+            <i class="ri-calendar-event-line"></i> event
+            <i class="ri-store-2-line"></i> vendor login
+        </p>
+    </div>
 </template>
 
 <script>
+import logoImg from "../assets/images/logo.png"
 export default {
-    name: "Navbar"
+    name: "Navbar",
+    data() {
+        return {
+            logoImg: logoImg,
+        };
+    }
 }
 </script>
