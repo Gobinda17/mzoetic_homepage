68f33e5e3cb904eab1ceffdac197bcd2910e8245
Gobinda17 <gobindadeb14@gmail.com>
2024-03-20 19:58:10 +0530

    Navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <template>
+  <div>
+    <navBar></navBar>
+  </div>
   <h1 class="text-rose-800">Hello</h1>
 </template>
 
 <script>
+import navBar from "../components/NavBar.vue"
 export default {
   name: "HomePage",
+  components: {
+    navBar
+  }
 }
 </script>
